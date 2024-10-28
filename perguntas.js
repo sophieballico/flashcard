79c3560f0321d1a609ee561a673e27959164c10e
sophieballico <144159880+sophieballico@users.noreply.github.com>
2024-10-28 08:15:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austria?',
+    'A capital da Austria é viena'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz mosca em Inglês?',
+    'Oi em ingles é FLY (FLAI)'
+)
